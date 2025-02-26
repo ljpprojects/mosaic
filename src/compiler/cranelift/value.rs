5daab6081caa4db2d27458f26325322343ce0796
Lucas Plumb <lucasplumb@icloud.com>
2025-02-26 11:51:25 +1100

    Add import and fix typo in compiler/cranelift/value.rs

--- src/compiler/cranelift/value.rs
+++ src/compiler/cranelift/value.rs
@@ -1,10 +1,10 @@
-use crate::utils::Indirection;
+use crate::utils::{Indirection, OneOf};
 use cranelift_codegen::ir::Value as RawValue;
 use crate::compiler::identifiers::StatementIdentifier;
 
 pub struct OwnershipTrace {
     /// Stores the name of the variable that owns the value, if any
-    owner: OneOf<String, String, Statementidentifier>
+    owner: OneOf<String, String, StatementIdentifier>
 }
 
 pub enum CraneliftValue {
